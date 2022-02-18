068ce193998b3865346ebaac13f20234e10b9b2b
julius.suominen <julius.suominen@gmail.com>
2022-02-18 11:51:31 +0200

    add cognito auth, search

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,6 +1,25 @@
 "use strict";
 
 const fs = require("fs").promises;
+const mustache = require("mustache");
+const http = require("superagent");
+const aws4 = require("aws4");
+const URL = require("url");
+
+const restaurantsApiRoot = process.env.restaurants_api;
+const awsRegion = process.env.AWS_REGION;
+const cognitoUserPoolId = process.env.cognito_user_pool_id;
+const cognitoClientId = process.env.cognito_client_id;
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 var html; // lambda reuses cached global vars
 
@@ -11,11 +30,38 @@ function loadHtml() {
   return html;
 }
 
+async function getRestaurants() {
+  const url = URL.parse(restaurantsApiRoot);
+  const opts = aws4.sign({
+    host: url.hostname,
+    path: url.pathname,
+  });
+  const { body } = await http
+    .get(restaurantsApiRoot)
+    .set("Host", opts.headers["Host"])
+    .set("X-Amz-Date", opts.headers["X-Amz-Date"])
+    .set("Authorization", opts.headers["Authorization"])
+    .set("X-Amz-Security-Token", opts.headers["X-Amz-Security-Token"]);
+  return body;
+}
+
 module.exports.handler = async (event) => {
-  let page = await loadHtml();
+  const page = await loadHtml();
+  const restaurants = await getRestaurants();
+  const dayOfWeek = days[new Date().getDay()];
+  const view = {
+    restaurants,
+    dayOfWeek,
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoClientId,
+    searchUrl: `${restaurantsApiRoot}/search`,
+  };
+  const rendered = mustache.render(page, view);
+
   return {
     statusCode: 200,
-    body: page,
+    body: rendered,
     headers: {
       "Content-Type": "text/html; charset=UTF-8",
     },
---
+++ functions/search-restaurants.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+const { defaultResults, restaurants_table } = process.env;
+
+async function findRestaurantsByTheme(theme, count) {
+  const req = {
+    TableName: restaurants_table,
+    Limit: count,
+    FilterExpression: "contains(themes, :theme)",
+    ExpressionAttributeValues: { ":theme": theme },
+  };
+  const res = await dynamodb.scan(req).promise();
+  return res.Items;
+}
+
+module.exports.handler = async (event, ctx) => {
+  const req = JSON.parse(event.body);
+  const restaurants = await findRestaurantsByTheme(req.theme, defaultResults);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(restaurants),
+  };
+  return response;
+};
